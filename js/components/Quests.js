94e65496d18eecbb70a81cdf4d229f637e07da55
unknown <dschu012@gmail.com>
2021-05-21 13:30:53 -0400

    more code cleanup

--- js/components/Quests.js
+++ js/components/Quests.js
@@ -1,18 +1,73 @@
+
 import html from '../html.js';
+
+const quests = [
+  {
+    key: "act_i", label: "Act I",
+    quests: [
+      { key: "den_of_evil", label: "Den Of Evil", values: [{ key: "is_completed", label: "Completed" }] },
+      { key: "sisters_burial_grounds", label: "Sisters' Burial Grounds", values: [{ key: "is_completed", label: "Completed" }] },
+      { key: "the_search_for_cain", label: "Search for Cain", values: [{ key: "unk10", label: "Cow King Killed" }, { key: "is_completed", label: "Completed" }] },
+      { key: "the_forgotten_tower", label: "The Forgotten Tower", values: [{ key: "is_completed", label: "Completed" }] },
+      { key: "tools_of_the_trade", label: "Tools of the Trade", values: [{ key: "is_completed", label: "Completed" }] },
+      { key: "sisters_to_the_slaughter", label: "Sisters to the Slaughter", values: [{ key: "is_completed", label: "Completed" }] },
+    ]
+  },
+  {
+    key: "act_ii", label: "Act II",
+    quests: [
+      { key: "radaments_lair", label: "Radament's Lair", values: [{ key: "is_completed", label: "Completed" }] },
+      { key: "the_horadric_staff", label: "The Horadric Staff", values: [{ key: "is_completed", label: "Completed" }] },
+      { key: "tainted_sun", label: "Tainted Sun", values: [{ key: "is_completed", label: "Completed" }] },
+      { key: "arcane_sanctuary", label: "Arcane Sanctuary", values: [{ key: "is_completed", label: "Completed" }] },
+      { key: "the_summoner", label: "The Summoner", values: [{ key: "is_completed", label: "Completed" }] },
+      { key: "the_seven_tombs", label: "The Seven Tombs", values: [{ key: "is_completed", label: "Completed" }] },
+    ]
+  },
+  {
+    key: "act_iii", label: "Act III",
+    quests: [
+      { key: "the_golden_bird", label: "The Golden Bird", values: [{ key: "is_completed", label: "Completed" }] },
+      { key: "blade_of_the_old_religion", label: "Blade of the Old Religion", values: [{ key: "is_completed", label: "Completed" }] },
+      { key: "khalims_will", label: "Khalim's Will", values: [{ key: "is_completed", label: "Completed" }] },
+      { key: "lam_esens_tome", label: "Lam Esen's Tome", values: [{ key: "is_completed", label: "Completed" }] },
+      { key: "the_blackened_temple", label: "The Blackened Temple", values: [{ key: "is_completed", label: "Completed" }] },
+      { key: "the_guardian", label: "The Guardian", values: [{ key: "is_completed", label: "Completed" }] },
+    ]
+  },
+  {
+    key: "act_iv", label: "Act IV",
+    quests: [
+      { key: "the_fallen_angel", label: "Fallen Angel", values: [{ key: "is_completed", label: "Completed" }] },
+      { key: "hellforge", label: "Hell's Forge", values: [{ key: "is_completed", label: "Completed" }] },
+      { key: "terrors_end", label: "Terror's End", values: [{ key: "is_completed", label: "Completed" }] },
+    ]
+  },
+  {
+    key: "act_v", label: "Act V",
+    quests: [
+      { key: "siege_on_harrogath", label: "Siege on Harrogath", values: [{ key: "is_completed", label: "Completed" }] },
+      { key: "rescue_on_mount_arreat", label: "Rescue on Mount Arreat", values: [{ key: "is_completed", label: "Completed" }] },
+      { key: "prison_of_ice", label: "Prison of Ice", values: [{ key: "consumed_scroll ", label: "Consumed Scroll" }, { key: "is_completed", label: "Completed" }] },
+      { key: "betrayal_of_harrogath", label: "Betrayal of Harrogath", values: [{ key: "is_completed", label: "Completed" }] },
+      { key: "rite_of_passage", label: "Rite of Passage", values: [{ key: "is_completed", label: "Completed" }] },
+      { key: "eve_of_destruction", label: "Eve of Destruction", values: [{ key: "is_completed", label: "Completed" }] },
+    ]
+  },
+];
 
 export default {
   template: html`
 <div class="form-row">
   <div class="col-md-4" v-for="difficulty in difficulties">
     <ul>
-      <li><i class="fa fa-angle-right rotate"></i> {{difficulty.value}}</li>
-      <ul class="col-md-offset-1" v-for="act in quests">
-        <li><i class="fa fa-angle-right rotate"></i> {{ act.label }}</li>
+      <li><label>{{ difficulty.label }}</label></li>
+      <ul class="col-md-offset-1" v-for="act in difficulty.acts">
+        <li><label>{{ act.label }}</label></li>
         <ul class="col-md-offset-2" v-for="quest in act.quests">
-          <li><i class="fa fa-angle-right rotate"></i>{{ quest.label }}</li>
+          <li><label>{{ quest.label }}</label></li>
           <ul class="col-md-offset-3">
-            <li v-for="state in quest.values"><label><input class="form-check-input" type="checkbox"
-                  v-model="save.header[difficulty.key][act.key][quest.key][state.key]">{{ state.label }}</label></li>
+            <li v-for="state in quest.values"><label><input class="form-check-input" type="checkbox" v-model="save.header[difficulty.key][act.key][quest.key][state.key]">{{ state.label }}</label></li>
           </ul>
         </ul>
       </ul>
@@ -25,61 +80,16 @@ export default {
   },
   data() {
     return {
-      difficulties: [{ key: 'quests_normal', value: "Normal" }, { key: 'quests_nm', value: "Nightmare" }, { key: 'quests_hell', value: "Hell" }],
-      quests: [
-        {
-          key: "act_i", label: "Act I",
-          quests: [
-            { key: "den_of_evil", label: "Den Of Evil", values: [{ key: "is_completed", label: "Completed" }] },
-            { key: "sisters_burial_grounds", label: "Sisters' Burial Grounds", values: [{ key: "is_completed", label: "Completed" }] },
-            { key: "the_search_for_cain", label: "Search for Cain", values: [{ key: "unk10", label: "Cow King Killed" }, { key: "is_completed", label: "Completed" }] },
-            { key: "the_forgotten_tower", label: "The Forgotten Tower", values: [{ key: "is_completed", label: "Completed" }] },
-            { key: "tools_of_the_trade", label: "Tools of the Trade", values: [{ key: "is_completed", label: "Completed" }] },
-            { key: "sisters_to_the_slaughter", label: "Sisters to the Slaughter", values: [{ key: "is_completed", label: "Completed" }] },
-          ]
-        },
-        {
-          key: "act_ii", label: "Act II",
-          quests: [
-            { key: "radaments_lair", label: "Radament's Lair", values: [{ key: "is_completed", label: "Completed" }] },
-            { key: "the_horadric_staff", label: "The Horadric Staff", values: [{ key: "is_completed", label: "Completed" }] },
-            { key: "tainted_sun", label: "Tainted Sun", values: [{ key: "is_completed", label: "Completed" }] },
-            { key: "arcane_sanctuary", label: "Arcane Sanctuary", values: [{ key: "is_completed", label: "Completed" }] },
-            { key: "the_summoner", label: "The Summoner", values: [{ key: "is_completed", label: "Completed" }] },
-            { key: "the_seven_tombs", label: "The Seven Tombs", values: [{ key: "is_completed", label: "Completed" }] },
-          ]
-        },
-        {
-          key: "act_iii", label: "Act III",
-          quests: [
-            { key: "the_golden_bird", label: "The Golden Bird", values: [{ key: "is_completed", label: "Completed" }] },
-            { key: "blade_of_the_old_religion", label: "Blade of the Old Religion", values: [{ key: "is_completed", label: "Completed" }] },
-            { key: "khalims_will", label: "Khalim's Will", values: [{ key: "is_completed", label: "Completed" }] },
-            { key: "lam_esens_tome", label: "Lam Esen's Tome", values: [{ key: "is_completed", label: "Completed" }] },
-            { key: "the_blackened_temple", label: "The Blackened Temple", values: [{ key: "is_completed", label: "Completed" }] },
-            { key: "the_guardian", label: "The Guardian", values: [{ key: "is_completed", label: "Completed" }] },
-          ]
-        },
-        {
-          key: "act_iv", label: "Act IV",
-          quests: [
-            { key: "the_fallen_angel", label: "Fallen Angel", values: [{ key: "is_completed", label: "Completed" }] },
-            { key: "hellforge", label: "Hell's Forge", values: [{ key: "is_completed", label: "Completed" }] },
-            { key: "terrors_end", label: "Terror's End", values: [{ key: "is_completed", label: "Completed" }] },
-          ]
-        },
-        {
-          key: "act_v", label: "Act V",
-          quests: [
-            { key: "siege_on_harrogath", label: "Siege on Harrogath", values: [{ key: "is_completed", label: "Completed" }] },
-            { key: "rescue_on_mount_arreat", label: "Rescue on Mount Arreat", values: [{ key: "is_completed", label: "Completed" }] },
-            { key: "prison_of_ice", label: "Prison of Ice", values: [{ key: "consumed_scroll ", label: "Consumed Scroll" }, { key: "is_completed", label: "Completed" }] },
-            { key: "betrayal_of_harrogath", label: "Betrayal of Harrogath", values: [{ key: "is_completed", label: "Completed" }] },
-            { key: "rite_of_passage", label: "Rite of Passage", values: [{ key: "is_completed", label: "Completed" }] },
-            { key: "eve_of_destruction", label: "Eve of Destruction", values: [{ key: "is_completed", label: "Completed" }] },
-          ]
-        },
-      ]
+      difficulties: [
+        { key: 'quests_normal', label: "Normal", acts: JSON.parse(JSON.stringify(quests)) }, 
+        { key: 'quests_nm', label: "Nightmare", acts: JSON.parse(JSON.stringify(quests)) }, 
+        { key: 'quests_hell', label: "Hell", acts: JSON.parse(JSON.stringify(quests)) }
+      ],
     };
+  },
+  methods: {
+    change(difficulty, act, quest) {
+      console.log(difficulty, act, quest);
+    }
   }
 };
--- js/components/Waypoints.js
+++ js/components/Waypoints.js
@@ -1,16 +1,82 @@
 import html from '../html.js';
 
+const waypoints = [
+  {
+    key: "act_i", label: "Act I",
+    waypoints: [
+      { key: "rogue_encampement", label: "Rogue Encampement" },
+      { key: "cold_plains", label: "Cold Plains" },
+      { key: "stony_field", label: "Stoney Field" },
+      { key: "dark_woods", label: "Dark Woods" },
+      { key: "black_marsh", label: "Black Marsh" },
+      { key: "outer_cloister", label: "Outer Cloister" },
+      { key: "jail_lvl_1", label: "Jail Lvl 1" },
+      { key: "inner_cloister", label: "Inner Cloister" },
+      { key: "catacombs_lvl_2", label: "Catacombs Lvl 2" },
+    ]
+  },
+  {
+    key: "act_ii", label: "Act II",
+    waypoints: [
+      { key: "lut_gholein", label: "Lut Gholein" },
+      { key: "sewers_lvl_2", label: "Sewers Lvl 2" },
+      { key: "dry_hills", label: "Dry Hills" },
+      { key: "halls_of_the_dead_lvl_2", label: "Halls of the Dead Lvl 2" },
+      { key: "far_oasis", label: "Far Oasis" },
+      { key: "lost_city", label: "Lost City" },
+      { key: "palace_cellar_lvl_1", label: "Palace Cellar Lvl 1" },
+      { key: "arcane_sanctuary", label: "Arcane Sanctuary" },
+      { key: "canyon_of_the_magi", label: "Canyon of the Magi" },
+    ]
+  },
+  {
+    key: "act_iii", label: "Act III",
+    waypoints: [
+      { key: "kurast_docks", label: "Kurast Docks" },
+      { key: "spider_forest", label: "Spider Forest" },
+      { key: "great_marsh", label: "Great Marsh" },
+      { key: "flayer_jungle", label: "Flayer Jungle" },
+      { key: "lower_kurast", label: "Lower Kurast" },
+      { key: "kurast_bazaar", label: "Kurast Bazaar" },
+      { key: "upper_kurast", label: "Upper Kurast" },
+      { key: "travincal", label: "Travincal" },
+      { key: "durance_of_hate_lvl_2", label: "Durance of Hate Lvl 2" },
+    ]
+  },
+  {
+    key: "act_iv", label: "Act IV",
+    waypoints: [
+      { key: "the_pandemonium_fortress", label: "Pandemonium Fortress" },
+      { key: "city_of_the_damned", label: "City of the Damned" },
+      { key: "river_of_flame", label: "River of Flame" },
+    ]
+  },
+  {
+    key: "act_v", label: "Act V",
+    waypoints: [
+      { key: "harrogath", label: "Harrogath" },
+      { key: "frigid_highlands", label: "Frigid Highlands" },
+      { key: "arreat_plateau", label: "Arreat Plateau" },
+      { key: "crystalline_passage", label: "Crystalline Passage" },
+      { key: "halls_of_pain", label: "Halls of Pain" },
+      { key: "glacial_trail", label: "Glacial Trail" },
+      { key: "frozen_tundra", label: "Frozen Tundra" },
+      { key: "the_ancients_way", label: "The Ancients' Way" },
+      { key: "worldstone_keep_lvl_2", label: "Worldstone Keep Lvl 2" },
+    ]
+  },
+];
+
 export default {
   template: html`
 <div class="form-row">
   <div class="col-md-4" v-for="difficulty in difficulties">
     <ul>
-      <li><i class="fa fa-angle-right rotate"></i> {{difficulty.value}}</li>
-      <ul class="col-md-offset-1" v-for="act in waypoints">
-        <li><i class="fa fa-angle-right rotate"></i> {{ act.label }}</li>
+      <li><label>{{difficulty.label}}</label></li>
+      <ul class="col-md-offset-1" v-for="act in difficulty.acts">
+        <li><label>{{ act.label }}</label></li>
         <ul class="col-md-offset-2" v-for="waypoint in act.waypoints">
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key][act.key][waypoint.key]">{{ waypoint.label }}</label></li>
+          <li><label><input class="form-check-input" type="checkbox" v-model="save.header.waypoints[difficulty.key][act.key][waypoint.key]">{{ waypoint.label }}</label></li>
         </ul>
       </ul>
     </ul>
@@ -22,73 +88,11 @@ export default {
   },
   data() {
     return {
-      difficulties: [{ key: 'normal', value: "Normal" }, { key: 'nm', value: "Nightmare" }, { key: 'hell', value: "Hell" }],
-      waypoints: [
-        {
-          key: "act_i", label: "Act I",
-          waypoints: [
-            { key: "rogue_encampement", label: "Rogue Encampement" },
-            { key: "cold_plains", label: "Cold Plains" },
-            { key: "stony_field", label: "Stoney Field" },
-            { key: "dark_woods", label: "Dark Woods" },
-            { key: "black_marsh", label: "Black Marsh" },
-            { key: "outer_cloister", label: "Outer Cloister" },
-            { key: "jail_lvl_1", label: "Jail Lvl 1" },
-            { key: "inner_cloister", label: "Inner Cloister" },
-            { key: "catacombs_lvl_2", label: "Catacombs Lvl 2" },
-          ]
-        },
-        {
-          key: "act_ii", label: "Act II",
-          waypoints: [
-            { key: "lut_gholein", label: "Lut Gholein" },
-            { key: "sewers_lvl_2", label: "Sewers Lvl 2" },
-            { key: "dry_hills", label: "Dry Hills" },
-            { key: "halls_of_the_dead_lvl_2", label: "Halls of the Dead Lvl 2" },
-            { key: "far_oasis", label: "Far Oasis" },
-            { key: "lost_city", label: "Lost City" },
-            { key: "palace_cellar_lvl_1", label: "Palace Cellar Lvl 1" },
-            { key: "arcane_sanctuary", label: "Arcane Sanctuary" },
-            { key: "canyon_of_the_magi", label: "Canyon of the Magi" },
-          ]
-        },
-        {
-          key: "act_iii", label: "Act III",
-          waypoints: [
-            { key: "kurast_docks", label: "Kurast Docks" },
-            { key: "spider_forest", label: "Spider Forest" },
-            { key: "great_marsh", label: "Great Marsh" },
-            { key: "flayer_jungle", label: "Flayer Jungle" },
-            { key: "lower_kurast", label: "Lower Kurast" },
-            { key: "kurast_bazaar", label: "Kurast Bazaar" },
-            { key: "upper_kurast", label: "Upper Kurast" },
-            { key: "travincal", label: "Travincal" },
-            { key: "durance_of_hate_lvl_2", label: "Durance of Hate Lvl 2" },
-          ]
-        },
-        {
-          key: "act_iv", label: "Act IV",
-          waypoints: [
-            { key: "the_pandemonium_fortress", label: "Pandemonium Fortress" },
-            { key: "city_of_the_damned", label: "City of the Damned" },
-            { key: "river_of_flame", label: "River of Flame" },
-          ]
-        },
-        {
-          key: "act_v", label: "Act V",
-          waypoints: [
-            { key: "harrogath", label: "Harrogath" },
-            { key: "frigid_highlands", label: "Frigid Highlands" },
-            { key: "arreat_plateau", label: "Arreat Plateau" },
-            { key: "crystalline_passage", label: "Crystalline Passage" },
-            { key: "halls_of_pain", label: "Halls of Pain" },
-            { key: "glacial_trail", label: "Glacial Trail" },
-            { key: "frozen_tundra", label: "Frozen Tundra" },
-            { key: "the_ancients_way", label: "The Ancients' Way" },
-            { key: "worldstone_keep_lvl_2", label: "Worldstone Keep Lvl 2" },
-          ]
-        },
-      ]
+      difficulties: [
+        { key: 'normal', label: "Normal", acts: JSON.parse(JSON.stringify(waypoints)) }, 
+        { key: 'nm', label: "Nightmare", acts: JSON.parse(JSON.stringify(waypoints)) }, 
+        { key: 'hell', label: "Hell", acts: JSON.parse(JSON.stringify(waypoints)) }
+      ],
     };
   }
 };
