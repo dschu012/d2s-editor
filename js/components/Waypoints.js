e3b847e4156b90e1bc03ac16790107b05c072449
unknown <dschu012@gmail.com>
2021-05-21 11:49:27 -0400

    refactor waypoints make it more managable

--- js/components/Waypoints.js
+++ js/components/Waypoints.js
@@ -6,115 +6,11 @@ export default {
   <div class="col-md-4" v-for="difficulty in difficulties">
     <ul>
       <li><i class="fa fa-angle-right rotate"></i> {{difficulty.value}}</li>
-      <ul class="col-md-offset-1">
-        <li><i class="fa fa-angle-right rotate"></i> Act I</li>
-        <ul class="col-md-offset-2">
+      <ul class="col-md-offset-1" v-for="act in waypoints">
+        <li><i class="fa fa-angle-right rotate"></i> {{ act.label }}</li>
+        <ul class="col-md-offset-2" v-for="waypoint in act.waypoints">
           <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_i.rogue_encampement">Rogue Encampement</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_i.cold_plains">Cold Plains</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_i.stony_field">Stoney Field</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_i.dark_woods">Dark Woods</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_i.black_marsh">Black Marsh</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_i.outer_cloister">Outer Cloister</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_i.jail_lvl_1">Jail Lvl 1</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_i.inner_cloister">Inner Cloister</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_i.catacombs_lvl_2">Catacombs Lvl 2</label></li>
-        </ul>
-      </ul>
-      <ul class="col-md-offset-1">
-        <li><i class="fa fa-angle-right rotate"></i> Act II</li>
-        <ul class="col-md-offset-2">
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_ii.lut_gholein">Lut Gholein</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_ii.sewers_lvl_2">Sewers Lvl 2</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_ii.dry_hills">Dry Hills</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_ii.halls_of_the_dead_lvl_2">Halls of the Dead Lvl
-              2</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_ii.far_oasis">Far Oasis</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_ii.lost_city">Lost City</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_ii.palace_cellar_lvl_1">Palace Cellar Lvl 1</label>
-          </li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_ii.arcane_sanctuary">Arcane Sanctuary</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_ii.canyon_of_the_magi">Canyon of the Magi</label>
-          </li>
-        </ul>
-      </ul>
-      <ul class="col-md-offset-1">
-        <li><i class="fa fa-angle-right rotate"></i> Act III</li>
-        <ul class="col-md-offset-2">
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_iii.kurast_docks">Kurast Docks</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_iii.spider_forest">Spider Forest</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_iii.great_marsh">Great Marsh</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_iii.flayer_jungle">Flayer Jungle</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_iii.lower_kurast">Lower Kurast</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_iii.kurast_bazaar">Kurast Bazaar</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_iii.upper_kurast">Upper Kurast</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_iii.travincal">Travincal</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_iii.durance_of_hate_lvl_2">Durance of Hate Lvl
-              2</label></li>
-        </ul>
-      </ul>
-      <ul class="col-md-offset-1">
-        <li><i class="fa fa-angle-right rotate"></i> Act IV</li>
-        <ul class="col-md-offset-2">
-          <li><label><input class="form-check-input" type="checkbox"
-                :checked="save.header.waypoints[difficulty.key].act_iv.the_pandemonium_fortress">Pandemonium
-              Fortress</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                :checked="save.header.waypoints[difficulty.key].act_iv.city_of_the_damned">City of the Damned</label>
-          </li>
-          <li><label><input class="form-check-input" type="checkbox"
-                :checked="save.header.waypoints[difficulty.key].act_iv.river_of_flame">River of Flames</label></li>
-        </ul>
-      </ul>
-      <ul class="col-md-offset-1">
-        <li><i class="fa fa-angle-right rotate"></i> Act V</li>
-        <ul class="col-md-offset-2">
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_v.harrogath">Harrogath</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_v.frigid_highlands">Frigid Highlands</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_v.arreat_plateau">Arreat Plateau</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_v.crystalline_passage">Crystalline Passage</label>
-          </li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_v.halls_of_pain">Halls of Pain</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_v.glacial_trail">Glacial Trail</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_v.frozen_tundra">Frozen Tundra</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_v.the_ancients_way">The Ancients' Way</label></li>
-          <li><label><input class="form-check-input" type="checkbox"
-                v-model="save.header.waypoints[difficulty.key].act_v.worldstone_keep_lvl_2">Worldstone Keep Lvl
-              2</label></li>
+                v-model="save.header.waypoints[difficulty.key][act.key][waypoint.key]">{{ waypoint.label }}</label></li>
         </ul>
       </ul>
     </ul>
@@ -126,7 +22,73 @@ export default {
   },
   data() {
     return {
-      difficulties: [{ key: 'normal', value: "Normal" }, { key: 'nm', value: "Nightmare" }, { key: 'hell', value: "Hell" }]
+      difficulties: [{ key: 'normal', value: "Normal" }, { key: 'nm', value: "Nightmare" }, { key: 'hell', value: "Hell" }],
+      waypoints: [
+        {
+          key: "act_i", label: "Act I",
+          waypoints: [
+            { key: "rogue_encampement", label: "Rogue Encampement" },
+            { key: "cold_plains", label: "Cold Plains" },
+            { key: "stony_field", label: "Stoney Field" },
+            { key: "dark_woods", label: "Dark Woods" },
+            { key: "black_marsh", label: "Black Marsh" },
+            { key: "outer_cloister", label: "Outer Cloister" },
+            { key: "jail_lvl_1", label: "Jail Lvl 1" },
+            { key: "inner_cloister", label: "Inner Cloister" },
+            { key: "catacombs_lvl_2", label: "Catacombs Lvl 2" },
+          ]
+        },
+        {
+          key: "act_ii", label: "Act II",
+          waypoints: [
+            { key: "lut_gholein", label: "Lut Gholein" },
+            { key: "sewers_lvl_2", label: "Sewers Lvl 2" },
+            { key: "dry_hills", label: "Dry Hills" },
+            { key: "halls_of_the_dead_lvl_2", label: "Halls of the Dead Lvl 2" },
+            { key: "far_oasis", label: "Far Oasis" },
+            { key: "lost_city", label: "Lost City" },
+            { key: "palace_cellar_lvl_1", label: "Palace Cellar Lvl 1" },
+            { key: "arcane_sanctuary", label: "Arcane Sanctuary" },
+            { key: "canyon_of_the_magi", label: "Canyon of the Magi" },
+          ]
+        },
+        {
+          key: "act_iii", label: "Act III",
+          waypoints: [
+            { key: "kurast_docks", label: "Kurast Docks" },
+            { key: "spider_forest", label: "Spider Forest" },
+            { key: "great_marsh", label: "Great Marsh" },
+            { key: "flayer_jungle", label: "Flayer Jungle" },
+            { key: "lower_kurast", label: "Lower Kurast" },
+            { key: "kurast_bazaar", label: "Kurast Bazaar" },
+            { key: "upper_kurast", label: "Upper Kurast" },
+            { key: "travincal", label: "Travincal" },
+            { key: "durance_of_hate_lvl_2", label: "Durance of Hate Lvl 2" },
+          ]
+        },
+        {
+          key: "act_iv", label: "Act IV",
+          waypoints: [
+            { key: "the_pandemonium_fortress", label: "Pandemonium Fortress" },
+            { key: "city_of_the_damned", label: "City of the Damned" },
+            { key: "river_of_flame", label: "River of Flame" },
+          ]
+        },
+        {
+          key: "act_v", label: "Act V",
+          waypoints: [
+            { key: "harrogath", label: "Harrogath" },
+            { key: "frigid_highlands", label: "Frigid Highlands" },
+            { key: "arreat_plateau", label: "Arreat Plateau" },
+            { key: "crystalline_passage", label: "Crystalline Passage" },
+            { key: "halls_of_pain", label: "Halls of Pain" },
+            { key: "glacial_trail", label: "Glacial Trail" },
+            { key: "frozen_tundra", label: "Frozen Tundra" },
+            { key: "the_ancients_way", label: "The Ancients' Way" },
+            { key: "worldstone_keep_lvl_2", label: "Worldstone Keep Lvl 2" },
+          ]
+        },
+      ]
     };
   }
 };
