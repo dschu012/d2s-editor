131c90597c323b18a8e1b6f5607acf90a76dc734
ziio2110 <ziio2110@126.com>
2022-12-09 16:55:08 +0800

    improve the user experience of the ItemStatsEditor

--- js/components/inventory/ItemStatsEditor.js
+++ js/components/inventory/ItemStatsEditor.js
@@ -5,38 +5,54 @@ import html from '../../html.js';
 export default {
   template: html`
 <div>
-  <div class="form-row">
-    <div class="col-md-2">Stat</div>
-    <div class="col-md-2">Value</div>
+  <div class="form-row align-items-center">
+    <div class="col-auto">
+      <label class="form-label">Filter of Stat Selector</label>
+    </div>
+    <div class="col-auto">
+      <input type="text" class="form-control" v-model="filterString"/>
+    </div>
   </div>
 
-  <div class="form-row" v-for="(stat, statIdx) in itemStats" :key="statIdx">
-    <div class="col-md-4">
-      <div class="form-row">
-        <div class="col-md-1"><button type="button" class="btn btn-link red" @click="remove(statIdx)">&times;</button>
+  <div class="form-row">
+    <div class="col-md-6">Stat</div>
+    <div class="col-md-6">Value</div>
+  </div>
+
+  <div class="form-row align-items-center" v-for="(stat, statIdx) in itemStats" :key="statIdx">
+    <div class="col-md-6">
+      <div class="form-row align-items-center">
+        <div class="col-2 col-md-1"><span class="text-danger" style="cursor: pointer" @click="remove(statIdx)">&times;</span>
         </div>
-        <div class="col-md-11">
+        <div class="col-10 col-md-1"><span class="small">{{statIdx}}</span></div>
+        <div class="col-md-10">
           <select class="form-control" :id="id + 'Stat' + statIdx" v-model.number="stat.id" @change="onChange">
-            <option v-for="(it, i) in stats" :value="i" :key="i">{{i}} - {{it.s}}</option>
+            <option v-for="(it, opIdx) in filteredStats(stat)" :value="it?.idx" :key="it?.idx" :disabled="it?.sB==null">{{descString(it)}}</option>
+            <!-- ^ 1. make the option text more meaningful -->
+            <!-- ^ 2. disable error options -->
           </select>
         </div>
       </div>
     </div>
-    <div class="col-md-2" v-for="idx in numValues(stat.id)">
-      <select class="form-control" :id="id + 'Stat' + statIdx + 'Index'+ idx" v-model.number="stat.values[idx-1]"
-        v-if="isClass(stat.id, idx)" @change="onChange">
-        <option v-for="(it, i) in classes" :value="i" :key="i">{{it.co}}</option>
-      </select>
-      <select class="form-control" :id="id + 'Stat' + statIdx + 'Index'+ idx" v-model.number="stat.values[idx-1]"
-        v-else-if="isClassSkill(stat.id, idx)" @change="onChange">
-        <option v-for="(it, i) in classes[stat.values[idx]].ts" :value="i" :key="i">{{it}}</option>
-      </select>
-      <select class="form-control" :id="id + 'Stat' + statIdx + 'Index'+ idx" v-model.number="stat.values[idx-1]"
-        v-else-if="isSkill(stat.id, idx)" @change="onChange">
-        <option v-for="it in skills" :value="it.i" :key="it.i">{{it.v.s}}</option>
-      </select>
-      <input type="number" class="form-control" :min="min(stat.id)" :max="max(stat.id)" @input="change(stat.id, stat.values, idx-1)"
-        :id="id + 'Stat' + statIdx + 'Index'+ idx" v-model.number="stat.values[idx-1]" v-else>
+    <div class="col-md-6">
+      <div class="form-row align-items-center">
+        <div class="col" v-for="valIdx in numValues(stat.id)">
+          <select class="form-control" :id="id + 'Stat' + statIdx + 'Index'+ valIdx" v-model.number="stat.values[valIdx-1]"
+            v-if="isClass(stat.id, valIdx)" @change="onChange">
+            <option v-for="(it, opIdx) in classes" :value="opIdx" :key="opIdx">{{it.co}}</option>
+          </select>
+          <select class="form-control" :id="id + 'Stat' + statIdx + 'Index'+ valIdx" v-model.number="stat.values[valIdx-1]"
+            v-else-if="isClassSkill(stat.id, valIdx)" @change="onChange">
+            <option v-for="(it, opIdx) in classes[stat.values[valIdx]].ts" :value="opIdx" :key="opIdx">{{it}}</option>
+          </select>
+          <select class="form-control" :id="id + 'Stat' + statIdx + 'Index'+ valIdx" v-model.number="stat.values[valIdx-1]"
+            v-else-if="isSkill(stat.id, valIdx)" @change="onChange">
+            <option v-for="it in skills" :value="it.i" :key="it.i">{{it?.v?.s}}</option>
+          </select>
+          <input type="number" class="form-control" :min="min(stat.id)" :max="max(stat.id)" @input="change(stat.id, stat.values, valIdx-1)"
+            :id="id + 'Stat' + statIdx + 'Index'+ valIdx" v-model.number="stat.values[valIdx-1]" v-else>
+        </div>
+      </div>
     </div>
   </div>
   <div class="form-row">
@@ -44,19 +60,40 @@ export default {
   </div>
 </div>
 `,
+
 props: {
   id: String,
   itemStats: Array
 },
+
 data() {
   return {
-    stats: window.constants_99.constants.magical_properties.filter(s => s != null && s.s != null),
+    filterString: "",
+    // ^ for filtering the stats
+
+    stats: window.constants_99.constants.magical_properties.map((it,idx)=>(Object.assign(it??{}, {idx: idx}))),
+    // ^
+    // 1.add the `idx` property for convenience
+    // 2.prevent `it` to be null
+
+    // [Deprecated]
+    // stats: window.constants_99.constants.magical_properties.filter(s => s != null && s.s != null),
+    // ^ should not filler, because the original index is important!
+
     skills: window.constants_99.constants.skills.map((e, i) => { return {i:i, v:e} }).filter(e => e.v != null && e.v.s != null)
             .sort((a, b) => { return a.v.s.localeCompare(b.v.s) }),
     classes: window.constants_99.constants.classes,
   }
 },
+
 methods: {
+  descString(it) {
+    return `${it?.idx} - ${it.s} - [${it.dP}]`;
+    // TODO: `${it.dF}` is also useful.
+  },
+  filteredStats(stat) {
+    return this.stats.filter(it=>it?.idx==stat.id||this.descString(it).toLowerCase().includes(this.filterString));
+  },
   onChange() {
     this.$emit('stat-change', this.itemStats);
   },
