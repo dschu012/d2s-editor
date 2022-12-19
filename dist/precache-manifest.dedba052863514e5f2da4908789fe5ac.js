04de344508e72d39536967950257b8090b211060
spg-stany <spg.stany@gmail.com>
2022-12-19 11:25:20 +0300

    add dist for deploy

---
+++ dist/precache-manifest.dedba052863514e5f2da4908789fe5ac.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c64514d7bb02437af74b667af7dd6bfb",
+    "url": "/constants.html"
+  },
+  {
+    "revision": "c24ac746950ea7ef424fe54bdf74a416",
+    "url": "/css/app.css"
+  },
+  {
+    "revision": "f33e19d1a2031e55d094df110bda15cc",
+    "url": "/css/theme.css"
+  },
+  {
+    "revision": "55571f13adc4edef72fae8ecdad48494",
+    "url": "/d2/constants_96.bundle.min.js"
+  },
+  {
+    "revision": "faa905ae0b8a845f5cd2d2a6b7ba1bb4",
+    "url": "/d2/constants_99.bundle.min.js"
+  },
+  {
+    "revision": "13b598db4193e39000adeeaf8a382e1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18f53f6edb8d167d1760",
+    "url": "/js/app.af882ea5.js"
+  },
+  {
+    "revision": "468965bb7f12d8a03482",
+    "url": "/js/chunk-vendors.2997a483.js"
+  },
+  {
+    "revision": "e1535f69f7261a5bd4250d8bf06df461",
+    "url": "/manifest.json"
+  }
+]);
