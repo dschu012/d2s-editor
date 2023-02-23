1a242db2f8b29582ee8a5071387090c759f4504d
sturovskiy <turovskiy@banki.ru>
2023-02-23 20:31:16 +0300

    compact editor

--- src/components/inventory/ItemEditor.vue
+++ src/components/inventory/ItemEditor.vue
@@ -9,90 +9,93 @@
         </span>
       </div>
     </div>
-    
+
     <div></div>
     <div class="form-row">
-      <div></div>
-      <div><Item :item.sync="item" clazz="item-edit"></Item></div>
-      
-      <ul className="d2p-ItemOptions">
+      <div>
+        <Item :item.sync="item" clazz="item-edit"></Item>
+      </div>
+
+      <ul className="ItemOptions">
         <span v-if="!item.simple_item">
           <li>
-            <label :for="id + 'Rarity'">Rarity</label>
-            <select class="form-control" :id="id + 'Rarity'" v-model.number="item.quality" @change="onEvent('update')">
-              <option v-for="rarity in rarities" :value="rarity.key" :key="rarity.key">{{rarity.value}}</option>
-            </select>
+            <label>Quality:
+              <select v-model.number="item.quality" @change="onEvent('update')">
+                <option v-for="rarity in rarities" :value="rarity.key" :key="rarity.key">{{ rarity.value }}</option>
+              </select>
+            </label>
           </li>
           <li>
-            <div class="form-row" v-if="item.quality == 4">
-              <div class="col-md-6">
-                <label :for="id + 'Prefix'">Prefix</label>
-                <select class="form-control" :id="id + 'Prefix'" v-model.number="item.magic_prefix_name" @change="onEvent('update')">
+            <div v-if="item.quality == 4">
+              <label>Prefix:
+                <select v-model.number="item.magic_prefix_name" @change="onEvent('update')">
                   <option value="0">None</option>
-                  <option v-for="s in prefixes" :value="s.v.n" :key="s.i">{{s.v.n}}</option>
+                  <option v-for="s in prefixes" :value="s.v.n" :key="s.i">{{ s.v.n }}</option>
                 </select>
-              </div>
-              <div class="col-md-6">
-                <label :for="id + 'Suffix'">Suffix</label>
-                <select class="form-control" :id="id + 'Suffix'" v-model.number="item.magic_suffix_name" @change="onEvent('update')">
+              </label>
+              <label>Suffix:
+                <select v-model.number="item.magic_suffix_name" @change="onEvent('update')">
                   <option value="0">None</option>
-                  <option v-for="s in suffixes" :value="s.v.n" :key="s.i">{{s.v.n}}</option>
+                  <option v-for="s in suffixes" :value="s.v.n" :key="s.i">{{ s.v.n }}</option>
                 </select>
-              </div>
+              </label>
             </div>
-            <div class="form-row" v-if="item.quality == 6 || item.quality == 8">
-              <div class="col-md-6">
-                <label :for="id + 'RareName1'">Rare Name 1</label>
-                <select class="form-control" :id="id + 'RareName1'" v-model.number="item.rare_name" @change="onEvent('update')">
-                  <option v-for="s in rare_names" :value="s.v.n" :key="s.i">{{s.v.n}}</option>
+            <div v-if="item.quality == 6 || item.quality == 8">
+              <label>Rare Name 1:
+                <select v-model.number="item.rare_name" @change="onEvent('update')">
+                  <option v-for="s in rare_names" :value="s.v.n" :key="s.i">{{ s.v.n }}</option>
                 </select>
-              </div>
-              <div class="col-md-6">
-                <label :for="id + 'RareName2'">Rare Name 2</label>
-                <select class="form-control" :id="id + 'RareName2'" v-model.number="item.rare_name2" @change="onEvent('update')">
-                  <option v-for="s in rare_names" :value="s.v.n" :key="s.i">{{s.v.n}}</option>
+              </label>
+              <label>Rare Name 2:
+                <select v-model.number="item.rare_name2" @change="onEvent('update')">
+                  <option v-for="s in rare_names" :value="s.v.n" :key="s.i">{{ s.v.n }}</option>
                 </select>
-              </div>
+              </label>
             </div>
-          </li>  
+          </li>
           <li>
             <div v-if="item.quality == 5">
-              <label :for="id + 'SetName'">Set Name</label>
-              <select class="form-control" :id="id + 'SetName'" v-model.number="item.set_id" @change="onEvent('update')">
-                <option v-for="s in set_items" :value="s.i" :key="s.i">{{s.v.n}}</option>
-              </select>
+              <label>Set Name:
+                <select v-model.number="item.set_id" @change="onEvent('update')">
+                  <option v-for="s in set_items" :value="s.i" :key="s.i">{{ s.v.n }}</option>
+                </select>
+              </label>
             </div>
           </li>
           <li>
-            <label for="id + 'ILvl'">Item Level</label>
-            <input type="number" class="form-control" :id="id + 'ILvl'" v-model.number="item.level" @input="onEvent('update')">
+            <label>Item Level:
+              <input type="number" v-model.number="item.level" @input="onEvent('update')">
+            </label>
           </li>
         </span>
 
         <li>
-          <label for="id + 'Base'">Base</label>
-          <select class="form-control" :id="id + 'Base'" v-model="item.type" @change="onEvent('update')"> 
-            <option v-for="s in basesByType(item.type)" :value="s[0]" :key="s[0]">{{s[1].n}}</option>
-          </select>
+          <label>Base:
+            <select v-model="item.type" @change="onEvent('update')">
+              <option v-for="s in basesByType(item.type)" :value="s[0]" :key="s[0]">{{ s[1].n }}</option>
+            </select>
+          </label>
         </li>
 
         <span v-if="!item.simple_item">
           <li>
             <div v-if="item.defense_rating">
-              <label :for="id + 'Def'">Defense</label>
-              <input type="number" class="form-control" :id="id + 'Def'" v-model.number="item.defense_rating" @input="onEvent('update')">
+              <label>Defense:
+                <input type="number" v-model.number="item.defense_rating" @input="onEvent('update')">
+              </label>
             </div>
           </li>
           <li>
             <div v-if="item.socketed">
-              <label :for="id + 'Sockets'">Sockets</label>
-              <input type="number" class="form-control" :id="id + 'Sockets'" v-model.number="item.total_nr_of_sockets" @input="onEvent('update')">
+              <label>Sockets:
+                <input type="number" v-model.number="item.total_nr_of_sockets" @input="onEvent('update')">
+              </label>
             </div>
           </li>
           <li>
             <div class="form-check form-check-inline">
               <label class="form-check-label"><input class="form-check-input" type="checkbox"
-                v-model.number="item.socketed" :true-value="1" :false-value="0">Socketed</label>
+                  v-model.number="item.socketed" :true-value="1" :false-value="0">Socketed</label>
             </div>
           </li>
           <li>
@@ -101,21 +104,9 @@
                 v-model.number="item.ethereal" :true-value="1" :false-value="0" @change="onEvent('update')">Ethereal</label>
             </div>
           </li>
-          <li>
-            <div class="form-check form-check-inline">
-              <label class="form-check-label"><input class="form-check-input" type="checkbox"
-                v-model.number="item.personalized" :true-value="1" :false-value="0" @change="onEvent('update')">Personalized</label>
-            </div>
-          </li>
-          <li>
-            <div v-if="item.personalized">
-              <label :for="id + 'PersonalName'">Personalized Name</label>
-              <input type="text" class="form-control" :id="id + 'PersonalName'" v-model="item.personalized_name" @input="onEvent('update')">
-            </div>
-          </li>
         </span>
-      </ul>    
-    </div>  
+      </ul>
+    </div>
 
     <span v-if="!item.simple_item">
       <div v-if="item.magic_attributes">
@@ -142,57 +133,57 @@
 </template>
 
 <script>
-  import Item from './Item.vue';
-  import ItemStatsEditor from './ItemStatsEditor.vue';
+import Item from './Item.vue';
+import ItemStatsEditor from './ItemStatsEditor.vue';
 
-  export default {
-    name: 'ItemEditor',
-    props: {
-      id: String,
-      item: Object,
-      location: Object,
+export default {
+  name: 'ItemEditor',
+  props: {
+    id: String,
+    item: Object,
+    location: Object,
+  },
+  components: {
+    Item,
+    ItemStatsEditor,
+  },
+  data() {
+    return {
+      rarities: [{ key: 1, value: 'Low' }, { key: 2, value: 'Normal' }, { key: 3, value: 'Superior' }, { key: 4, value: 'Magic' }, { key: 5, value: 'Set' }, { key: 6, value: 'Rare' }, { key: 7, value: 'Unique' }, { key: 8, value: 'Crafted' }],
+      locations: [{ key: 0, value: 'Stored' }, { key: 1, value: 'Equipped' }, { key: 4, value: 'Cursor' }],
+      equipped_locations: [{ key: 1, value: 'Head' }, { key: 2, value: 'Neck' }, { key: 3, value: 'Torso' }, { key: 4, value: 'Right Hand' }, { key: 5, value: 'Left Hand' }, { key: 6, value: 'Right Finger' }, { key: 7, value: 'Left Finger' }, { key: 8, value: 'Waist' }, { key: 9, value: 'Boots' }, { key: 10, value: 'Gloves' }, { key: 11, value: 'Alternate Right Hand' }, { key: 12, value: 'Alternate Left Hand' }],
+      storage_pages: [{ key: 1, value: 'Inventory' }, { key: 4, value: 'Cube' }, { key: 5, value: 'Stash' }],
+      prefixes: window.constants.constants.magic_prefixes.map((e,i)=> { return { i:i, v:e }}).filter(e => e.v != null && e.v.n != null),
+      suffixes: window.constants.constants.magic_suffixes.map((e,i)=> { return { i:i, v:e }}).filter(e => e.v != null && e.v.n != null),
+      rare_names: window.constants.constants.rare_names.map((e,i)=> { return { i:i, v:e }}).filter(e => e.v != null && e.v.n != null),
+      unq_items: window.constants.constants.unq_items.map((e,i)=> { return { i:i, v:e }}).filter(e => e.v != null && e.v.n != null),
+      set_items: window.constants.constants.set_items.map((e,i)=> { return { i:i, v:e }}).filter(e => e.v != null && e.v.n != null),
+      armor_items: Object.entries(window.constants.constants.armor_items).filter(e => e[1].n != null),
+      weapon_items: Object.entries(window.constants.constants.weapon_items).filter(e => e[1].n != null),
+      other_items: Object.entries(window.constants.constants.other_items).filter(e => e[1].n != null),
+    };
+  },
+  methods: {
+    onEvent(type) {
+      this.$emit('item-event', { item: this.item, type: type });
     },
-    components: {
-      Item,
-      ItemStatsEditor,
+    onChildEvent(e) {
+      this.$emit('item-event', { item: e.item, type: e.type });
     },
-    data() {
-      return {
-        rarities: [{ key: 1, value: 'Low' }, { key: 2, value: 'Normal' }, { key: 3, value: 'Superior' }, { key: 4, value: 'Magic' }, { key: 5, value: 'Set' }, { key: 6, value: 'Rare' }, { key: 7, value: 'Unique' }, { key: 8, value: 'Crafted' }],
-        locations: [{ key: 0, value: 'Stored' }, { key: 1, value: 'Equipped' }, { key: 4, value: 'Cursor' }],
-        equipped_locations: [{ key: 1, value: 'Head' }, { key: 2, value: 'Neck' }, { key: 3, value: 'Torso' }, { key: 4, value: 'Right Hand' }, { key: 5, value: 'Left Hand' }, { key: 6, value: 'Right Finger' }, { key: 7, value: 'Left Finger' }, { key: 8, value: 'Waist' }, { key: 9, value: 'Boots' }, { key: 10, value: 'Gloves' }, { key: 11, value: 'Alternate Right Hand' }, { key: 12, value: 'Alternate Left Hand' }],
-        storage_pages: [{ key: 1, value: 'Inventory' }, { key: 4, value: 'Cube' }, { key: 5, value: 'Stash' }],
-        prefixes: window.constants.constants.magic_prefixes.map((e,i)=> { return { i:i, v:e }}).filter(e => e.v != null && e.v.n != null),
-        suffixes: window.constants.constants.magic_suffixes.map((e,i)=> { return { i:i, v:e }}).filter(e => e.v != null && e.v.n != null),
-        rare_names: window.constants.constants.rare_names.map((e,i)=> { return { i:i, v:e }}).filter(e => e.v != null && e.v.n != null),
-        unq_items: window.constants.constants.unq_items.map((e,i)=> { return { i:i, v:e }}).filter(e => e.v != null && e.v.n != null),
-        set_items: window.constants.constants.set_items.map((e,i)=> { return { i:i, v:e }}).filter(e => e.v != null && e.v.n != null),
-        armor_items: Object.entries(window.constants.constants.armor_items).filter(e => e[1].n != null),
-        weapon_items: Object.entries(window.constants.constants.weapon_items).filter(e => e[1].n != null),
-        other_items: Object.entries(window.constants.constants.other_items).filter(e => e[1].n != null),
-      };
+    onMove() {
+      this.$emit('item-event', { item: this.item, location: this.location, type: 'move' });
     },
-    methods: {
-      onEvent(type) {
-        this.$emit('item-event', { item: this.item, type: type });
-      },
-      onChildEvent(e) {
-        this.$emit('item-event', { item: e.item, type: e.type });
-      },
-      onMove() {
-        this.$emit('item-event', { item: this.item, location: this.location, type: 'move' });
-      },
-      basesByType(type) {
-        if (this.item.type_id == 3) {
-          return this.weapon_items.filter(e => e[1].nc == type || e[1].exc == type || e[1].elc == type)
-        } else if (this.item.type_id == 1) {
-          return this.armor_items.filter(e => e[1].nc == type || e[1].exc == type || e[1].elc == type)
-        } else if (this.item.type_id == 4) {
-          return this.other_items
-        } else {
-          return []
-        }
+    basesByType(type) {
+      if (this.item.type_id == 3) {
+        return this.weapon_items.filter(e => e[1].nc == type || e[1].exc == type || e[1].elc == type)
+      } else if (this.item.type_id == 1) {
+        return this.armor_items.filter(e => e[1].nc == type || e[1].exc == type || e[1].elc == type)
+      } else if (this.item.type_id == 4) {
+        return this.other_items
+      } else {
+        return []
       }
     }
-  };  
+  }
+};  
 </script>
