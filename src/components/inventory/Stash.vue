bfba30e9bae6f89e21f9207a453fdad8d99655dc
stanislav <spg_stany@gmail.com>
2024-02-18 04:33:44 +0300

    add RO shared stash

--- src/components/inventory/Stash.vue
+++ src/components/inventory/Stash.vue
@@ -11,70 +11,53 @@
           <button type="button" class="btn btn-secondary" :class="{ active: activeTab == 4 }"
             @click="changeTab(4)">Shared</button> 
         </div>
-        <div class="col-md-2">
-          <div class="float-right">
-            <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown"></button>
-            <div class="dropdown-menu dropdown-menu-right">
-              <div class="p-3 form-group">
-                <div class="form-row">
-                  <div class="col-md-12">
-                    <label>Shared Stash</label>
-                    <div class="input-group">
-                      <input type="number" min="1" max="20" class="form-control" v-model.number="grid.stash.w"
-                        @input="gridChange">
-                      <div class="input-group-prepend input-group-append">
-                        <div class="input-group-text">,</div>
-                      </div>
-                      <input type="number" min="1" max="20" class="form-control" v-model.number="grid.stash.h"
-                        @input="gridChange">
-                    </div>
-                  </div>
-                </div> 
-              </div>
-            </div>
-          </div>
-        </div>
+
       <Grid v-if="activeTab == 1" :width="grid.stash.w" :height="grid.stash.h" :page="1"
-        :items.sync="stash(0)" @item-selected="onSelect" @item-event="onEvent" :id="'Grid'" :contextMenu="$refs.contextMenu"></Grid>  
+        :items.sync="stash(0)" @item-selected="onSelect" @item-event="onEvent" :id="'Grid'" :contextMenu=contextMenu></Grid>  
       <Grid v-if="activeTab == 2" :width="grid.stash.w" :height="grid.stash.h" :page="2"
-        :items.sync="stash(1)" @item-selected="onSelect" @item-event="onEvent" :id="'Grid'" :contextMenu="$refs.contextMenu"></Grid>
+        :items.sync="stash(1)" @item-selected="onSelect" @item-event="onEvent" :id="'Grid'" :contextMenu=contextMenu></Grid>
       <Grid v-if="activeTab == 3" :width="grid.stash.w" :height="grid.stash.h" :page="3"
-        :items.sync="stash(2)" @item-selected="onSelect" @item-event="onEvent" :id="'Grid'" :contextMenu="$refs.contextMenu"></Grid>
+        :items.sync="stash(2)" @item-selected="onSelect" @item-event="onEvent" :id="'Grid'" :contextMenu=contextMenu></Grid>
       <Grid v-if="activeTab == 4" :width="grid.stash.w" :height="grid.stash.h" :page="4"
-        :items.sync="stash(3)" @item-selected="onSelect" @item-event="onEvent" :id="'Grid'" :contextMenu="$refs.contextMenu"></Grid>
+        :items.sync="stash(3)" @item-selected="onSelect" @item-event="onEvent" :id="'Grid'" :contextMenu=contextMenu></Grid>
     </div>
   </div>
 </template>
 
 <script>
-import Item from './Item.vue';
-import Grid from './Grid.vue';
+  import Item from './Item.vue';
+  import Grid from './Grid.vue';
+  import ContextMenu from "../ContextMenu.vue";
 
-export default {
-  name: 'Stash',
-  components: {
-  Item,
-  Grid },
-  data() {
-    return {
-      activeTab: 1,
-      grid: { stash: { w: 10, h: 10 } },
-    };
-  },
-  props: {
-    items: Object,
-  },
-  computed: {
+  export default {
+    name: 'Stash',
+    components: {
+      Item,
+      Grid,
+      ContextMenu
+    },
+    data() {
+      return {
+        activeTab: 1,
+        grid: { stash: { w: 10, h: 10 } },
+      };
+    },
+    props: {
+      items: Array,
+      id: String,
+      contextMenu: Object,
+    },
+    computed: {
 
-  },
-  methods: {
-    onSelect(item) {
-      this.$emit('item-selected', item);
     },
-    stash(i) { return this.items.pages[i].items || [] },
-    changeTab(i) {
-      this.activeTab = i;
-    },
+    methods: {
+      onSelect(item) {
+        this.$emit('item-selected', item);
+      },
+      stash(i) { return this.items.pages[i].items || [] },
+      changeTab(i) {
+        this.activeTab = i;
+      },
+    }
   }
-}
 </script>
